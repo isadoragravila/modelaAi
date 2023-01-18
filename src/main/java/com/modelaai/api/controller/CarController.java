a5746ea4854738a33844466017d28f26a69463e2
Isadora Ávila <isadorgravila@gmail.com>
2023-01-18 13:29:50 -0300

    feat: create findAll method to list all cars

--- src/main/java/com/modelaai/api/controller/CarController.java
+++ src/main/java/com/modelaai/api/controller/CarController.java
@@ -1,6 +1,9 @@
 package com.modelaai.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +23,10 @@ public class CarController {
   @PostMapping
   public void createCar(@RequestBody CarDTO req) {
     repository.save(new Car(req));
+  }
+
+  @GetMapping
+  public List<Car> listAll() {
+    return repository.findAll();
   }
 }
