9c6ce935fe9ba51f42e6b0c04bec6a2f2583e047
Isadora Ávila <isadorgravila@gmail.com>
2023-01-18 13:03:53 -0300

    feat: create car controller and car DTO

---
+++ src/main/java/com/modelaai/api/controller/CarController.java
@@ -0,0 +1,22 @@
+package com.modelaai.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.modelaai.api.dto.CarDTO;
+
+@RestController
+@RequestMapping("/api/car")
+public class CarController {
+  
+  @PostMapping
+  public void createCar(@RequestBody CarDTO req) {
+    System.out.println(req.modelo());
+    System.out.println(req.fabricante());
+    System.out.println(req.dataFabricacao());
+    System.out.println(req.valor());
+    System.out.println(req.anoModelo());
+  }
+}
